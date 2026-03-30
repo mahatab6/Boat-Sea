632bac40aee67cf97b35762236f3443f93887621
mahatav6 <mahatabcall56@gmail.com>
2026-03-30 12:08:34 +0600

    FeaturedBoat section added

--- src/components/modules/homePage/homePage.tsx
+++ src/components/modules/homePage/homePage.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Testimonials from "./testimonials";
 import HowItWorks from "./howItWorks";
 import PopularRoutes from "./popularRoutes";
+import FeaturedBoat from "./featuredBoat";
 
 const HomePage = () => {
   return (
     <main>
       <h2>Home page</h2>
+      <FeaturedBoat/>
       <PopularRoutes/>
       <HowItWorks/>
       <Testimonials />
